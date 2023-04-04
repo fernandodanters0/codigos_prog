49789f9833060255b0f69165565449139f16dfde
fernandodanters0 <julio@Julios-MacBook-Air.local>
2023-04-03 20:25:57 -0600

    mi segundo archivo en git

---
+++ Paquete/hola.java
@@ -0,0 +1,16 @@
+package Paquete;
+
+public class hola {
+
+    public void holaa(){
+        System.out.println("Hola void");
+    }
+
+    public static void main(String [] args) {
+
+        hola obj1 = new hola();
+        obj1.holaa();
+        
+    }
+    
+}
